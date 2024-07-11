43378c304b16e1f4f20a4131fa0f0c46db63db0b
SoftDev-Abay <abay.aliev.03@gmail.com>
2024-07-11 13:03:04 +0500

    minor changes

--- app/utils/http.ts
+++ app/utils/http.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "https://api.eurasia.crocos.kz/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 export default http;
